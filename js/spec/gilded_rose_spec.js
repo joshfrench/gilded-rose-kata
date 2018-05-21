af24d4f2e8498931506b0f2c939b4f184ea15925
Josh French <joshfrench@gmail.com>
2018-05-21 12:00:13 -0700

    Describe current behaviors

--- js/spec/gilded_rose_spec.js
+++ js/spec/gilded_rose_spec.js
@@ -1,9 +1,32 @@
 describe("Gilded Rose", function() {
 
-  it("should foo", function() {
-    items = [ new Item("foo", 0, 0) ];
-    update_quality();
-    expect(items[0].name).toEqual("fixme");
-  });
+  describe("update_quality()", () => {
 
+    describe("common behaviors", () => {
+      it("should decrease quality");
+      it("should decrease sell-in");
+      it("should decrease quality by 2 if past sell-by");
+      it("should not decrease quality < 0");
+    });
+
+    describe("Brie", () => {
+      it("should increase quality");
+      it("should not increase quality > 50");
+    });
+
+    describe("Backstage", () => {
+      it("should increase quality");
+      it("should increase quality by 2 when < 10 days left");
+      it("should increase quality by 3 when < 5 days left");
+      it("should decrease quality to 0 when past sell-by")
+      it("should not increase quality > 50");
+    });
+
+    describe("Sulfuras", () => {
+      it("should not decrease sell-in");
+      it("should not decrease quality");
+    });
+
+    xdescribe("Conjured items");
+  })
 });
