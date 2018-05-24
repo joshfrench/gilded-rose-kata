a86d022a57ff8fc38615e6df79d775bd4c102459
Josh French <joshfrench@gmail.com>
2018-05-23 20:52:49 -0700

    Use some threaded-friendly helpers
    Streamlines individual multimethod bodies, keeping them focused on the
    differences between item types

--- clojure/src/gilded_rose/core.clj
+++ clojure/src/gilded_rose/core.clj
@@ -1,46 +1,45 @@
 (ns gilded-rose.core)
 
+(defn decr-sell-in [item]
+  (update item :sell-in dec))
+
+(defn check-bounds [{:keys [quality] :as item}]
+  (assoc item :quality (-> quality (max 0) (min 50))))
+
 (defmulti update-item (fn [{:keys [name]}] name))
 
 (defmethod update-item :default [{:keys [sell-in quality] :as item}]
-  (assoc item :sell-in (dec sell-in)
-              :quality (max (- quality (if (> sell-in 0) 1 2))
-                            0)))
+  (-> item
+      decr-sell-in
+      (assoc :quality (- quality (if (> sell-in 0) 1 2)))
+      check-bounds))
 
-(defmethod update-item "Aged Brie" [{:keys [sell-in quality] :as item}]
-  (assoc item :sell-in (dec sell-in)
-              :quality (min (inc quality)
-                            50)))
+(defmethod update-item "Aged Brie" [item]
+  (-> item
+      decr-sell-in
+      (update :quality inc)
+      check-bounds))
 
 (defmethod update-item "Backstage passes to a TAFKAL80ETC concert" [{:keys [sell-in quality] :as item}]
-  (assoc item :sell-in (dec sell-in)
-              :quality (min 50
-                            (cond (<= sell-in 0)  0
-                                  (<= sell-in 5)  (+ 3 quality)
-                                  (<= sell-in 10) (+ 2 quality)
-                                  :else           (+ 1 quality)))))
+  (-> item
+      decr-sell-in
+      (assoc :quality (condp >= sell-in
+                             0  0
+                             5  (+ 3 quality)
+                             10 (+ 2 quality)
+                                (+ 1 quality)))
+      check-bounds))
 
 (defmethod update-item "Sulfuras, Hand of Ragnaros" [item] item)
 
 (defmethod update-item "Conjured Mana Cake" [{:keys [sell-in quality] :as item}]
-  (assoc item :sell-in (dec sell-in)
-              :quality (max (- quality (if (> sell-in 0) 2 4))
-                            0)))
+  (-> item
+      decr-sell-in
+      (assoc :quality (- quality (if (> sell-in 0) 2 4)))
+      check-bounds))
 
 (defn update-quality [items]
   (map update-item items))
 
-(defn item [item-name, sell-in, quality]
-  {:name item-name, :sell-in sell-in, :quality quality})
-
-(defn update-current-inventory[]
-  (let [inventory 
-    [
-      (item "+5 Dexterity Vest" 10 20)
-      (item "Aged Brie" 2 0)
-      (item "Elixir of the Mongoose" 5 7)
-      (item "Sulfuras, Hand of Ragnaros" 0 80)
-      (item "Backstage passes to a TAFKAL80ETC concert" 15 20)
-    ]]
-    (update-quality inventory)
-    ))
+(defn item [item-name sell-in quality]
+  {:name item-name :sell-in sell-in :quality quality})
